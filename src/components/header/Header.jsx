cbd22dbaa5bd3443c89280a8b848b614151ddbc7
Aakarsh Goyal <aakarshgoyal23@gmail.com>
2024-06-01 19:27:38 +0530

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
     return (
         <header className="header">
             <nav className="nav container">
-                <a href="index.html" className="nav_logo">Aakarsh</a>
+                <a href="/" className="nav_logo">Aakarsh</a>
 
                 <div className={Toggle ? "nav_menu show_menu" : "nav_menu"}>
 
