20234bbd2b0ac0fa63b01af8b407171a423e621b
Aakarsh Goyal <aakarshgoyal23@gmail.com>
2024-06-01 18:59:16 +0530

    Update skills.jsx

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -6,7 +6,7 @@ const Projects = () => {
   return (
     <section className="projects section" id='projects'>
         <h2 className="section_title">Projects</h2>
-        <span className='section_subtitle'>What I've built</span>
+        <span className='section_subtitle'>I've built</span>
 
         <Project/>
 
